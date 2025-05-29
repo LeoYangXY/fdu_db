26afbe0412ddb38c3a80dbdfabf2fe02b8faaa34
Leo <2236743065@qq.com>
2025-05-30 00:40:17 +0800

    删除tmp

--- wechat_db/settings.py
+++ wechat_db/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "tmp",
     'system_admin',   # 管理员功能
     'student',
     'teacher',
